c2b32168a4cf29b5a6ac42373e0615f15ff1a1b1
Romain on Carlo <romain.boman@gmail.com>
2015-08-02 08:03:59 +0200

    ajout code csharp

---
+++ csharp/console/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿// A Hello World! program in C#. 
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
