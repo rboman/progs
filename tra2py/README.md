1f3eef6a992b62827538b5bae10851cf942b57d4
Romain on Carlo <romain.boman@gmail.com>
2015-08-02 09:17:31 +0200

    cleaning tra2py

--- tra2py/README.md
+++ tra2py/README.md
@@ -1,3 +1,17 @@
 # tra2py
 
-convert "tra" files to a python file readable by Metafor
+  - extract 2 columns from semicolon-separated values
+  - write the 2 columns in a file
+  - display the resulting curve with Gnuplot (if available)
+
+## install
+install gnuplot for python
+ - [gnuplot](http://www.gnuplot.info/)
+ - [gnuplot-py](https://pypi.python.org/pypi/gnuplot-py)
+
+## run
+```
+./tra2py.py
+```
+
+![Screenshot](screenshot.png)
--- tra2py/tra2py.py
+++ tra2py/tra2py.py
@@ -1,29 +1,37 @@
-#
-# $Id$
+# tra2py
+#    - extract 2 columns from semicolon-separated values
+#    - write the 2 columns in a file
+#    - display the resulting curve with gnuplot (if available)
 
 
-
-def tra2py(file,fctname,rowX,rowY):
-    """ expl: convert('PLAFOS1', 'ecrou', 1, 3)"""
+def tra2py(file, fctname, rowX, rowY):
+    """ usage: tra2py('PLAFOS1', 'ecrou', 1, 3)"""
     import re
-    import Gnuplot, Gnuplot.funcutils    
-    g = Gnuplot.Gnuplot(debug=1)
-    g.title('Ecrouissage - fichier ' + file + '.tra') # (optional)
-    g('set data style lines')
-    g('set grid')
-    g('set xlabel \"epsilon\"')
-    g('set ylabel \"sigma\"')
+    
+    withgplot=True
+    try:
+        import Gnuplot, Gnuplot.funcutils    
+        g = Gnuplot.Gnuplot(debug=1)
+        g.title('File ' + file + '.tra')
+        g('set data style lines')
+        g('set grid')
+        g('set xlabel \"X\"')
+        g('set ylabel \"Y\"')
+    except ImportError:
+        print "\n** WARNING: Gnuplot.py not available!\n"
+        withgplot=False
+        
     myplot=[]
+    outname="output.txt"
     
     input = open(file + ".TRA", "r")
-    output = open(file+ ".py", "w")
-    
+    output = open(outname, "w")
     
     # skip 3 first lines
        
     for i in range(1,4):
         line = input.readline()
-        #output.write('# ' + line)   
+           
     # read the names
 
     prog = re.compile("[^;]+")
@@ -39,27 +47,31 @@ def tra2py(file,fctname,rowX,rowY):
 
     # reads the curves
     
-    #output.write(fctname + '=Funct()\n')
-
     line = input.readline()
     while line:
-    #for pipo in range(1,10):
         result = prog.findall(line)
-        #print len(result)
         if result:
-               x = result[rowX].replace(",",".")
-               y = result[rowY].replace(",",".")
-               myplot.append([float(x),float(y)])
-               #out = '%s.setData(%s,%s)\n' % (fctname, x, y)
-               out = '%s %s\n' % (x, y)
-               output.write(out)
+            x = result[rowX].replace(",",".")
+            y = result[rowY].replace(",",".")
+            myplot.append([float(x),float(y)])
+            out = '%s %s\n' % (x, y)
+            output.write(out)
         line = input.readline()      
-
+        
     input.close()                    
-    output.close()                   
-    g.plot(myplot)
-    raw_input('[ENTER]\n')
+    output.close() 
+    
+    print "output written to %s" % outname 
+    
+    if withgplot:
+        try:              
+            g.plot(myplot)
+        except:
+            raw_input("\n**WARNING: gnuplot.exe should be in the PATH!\n")
+
+    raw_input('\n[ENTER]\n') # otherwise gnuplot exits immediatly
 
 
-tra2py('PLAFOS4', 'ecrou', 1, 2)
+if __name__=="__main__":
+    tra2py('PLAFOS4', 'ecrou', 1, 2)
 
