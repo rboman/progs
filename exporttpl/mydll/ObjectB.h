6813856ecd648afc77837e99c637360326ea314c
Romain Boman <romain.boman@gmail.com>
2015-09-29 09:03:26 +0200

    modifs vbox

--- exporttpl/mydll/ObjectB.h
+++ exporttpl/mydll/ObjectB.h
@@ -5,7 +5,8 @@
 #include "mydll.h"
 #include "ObjectA.h"
 
-extern template class MYDLL_API ObjectA<int>;
+//extern template class MYDLL_API ObjectA<int>;  // VC: C4910 '__declspec(dllexport)' and 'extern' are incompatible on an explicit instantiation / MinGW: inline mult defined 
+extern template class ObjectA<int>;              // VC: C4661 no suitable definition provided for explicit template instantiation request  / MinGW OK - Intel OK
 
 class MYDLL_API ObjectB : public ObjectA<int>
 {
