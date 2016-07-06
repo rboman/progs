7430b75ca83a6e6479c35865e3207e411ef515c0
Romain Boman <romain.boman@gmail.com>
2016-07-06 09:22:27 +0200

    suppression timer update => slots

--- fractale/mandel.py
+++ fractale/mandel.py
@@ -2,11 +2,12 @@
 # -*- coding: latin-1; -*-
 
 import sys
-from PyQt4 import QtCore, QtGui
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
 
-class RenderThread(QtCore.QThread):
-    def __init__(self, parent=None, width=100, height=100):
-        QtCore.QThread.__init__(self, parent)
+class RenderThread(QThread):
+    def __init__(self, parent, width=100, height=100):
+        QThread.__init__(self, parent)
         self.wx = width
         self.wy = height
         self.inc = 1
@@ -16,22 +17,22 @@ class RenderThread(QtCore.QThread):
         self.xc = 0.0
         self.yc = 0.0
         self.nbc = 50
-        self.pixmap = QtGui.QPixmap(width, height)
-        self.mutex = QtCore.QMutex()
+        self.pixmap = QPixmap(width, height)
+        self.mutex = QMutex()
         self.abort = False
-
+        
     def run(self):
-        #print "starting thread on %dx%d" % (self.wx, self.wy) 
+        print "starting thread on %dx%d" % (self.wx, self.wy) 
         self.mutex.lock()  
-        painter = QtGui.QPainter(self.pixmap)
-        painter.fillRect(self.pixmap.rect(), QtCore.Qt.black)
+        painter = QPainter(self.pixmap)
+        painter.fillRect(self.pixmap.rect(), Qt.black)
         self.mutex.unlock()  
 
         # setup palette
         colours = []
         for n in range(self.nbc):
-            colours.append(QtGui.QColor(255*n/self.nbc, 0, 0))
-        colours.append(QtGui.QColor(0, 0, 0))
+            colours.append(QColor(255*n/self.nbc, 0, 0))
+        colours.append(QColor(0, 0, 0))
                
         for xe in range(0, self.wx, self.inc):
             for ye in range(0, self.wy, self.inc):
@@ -48,43 +49,40 @@ class RenderThread(QtCore.QThread):
                 self.mutex.unlock()
                 if self.abort:
                     return
+            self.emit(SIGNAL("newStuff()"))
+
         #print "thread is over!"
+        
     def transfo(self,xe,ye):
         x = self.ox + float(xe-self.wx/2)/self.zoom
         y = self.oy - float(ye-self.wy/2)/self.zoom
         return x,y         
 
-class MandelWindow(QtGui.QWidget):
+class MandelWindow(QWidget):
     """
     Main Qt Widget
     """
     def __init__(self, parent = None):
-        QtGui.QWidget.__init__(self, parent)
-        self.setWindowTitle("Mandelbrot")
-        self.resize(320, 200)
-        self.thread = RenderThread()
-        self.timer = QtCore.QTimer(self)
-        self.connect(self.timer, QtCore.SIGNAL("timeout()"), self.timerfct)
-        self.timer.start(10)
-        self.mouse = QtCore.QPoint()
+        QWidget.__init__(self, parent)
+        self.setWindowTitle("Mandelbrot (QThread)")
+        self.thread = None
+
+        self.mouse = QPoint()
         self.setMouseTracking(True) # appelle mouseMoveEvent meme si pas de clic
         
-    def timerfct(self):
-        self.update()
-        import random
-        print "timer()" , random.random()
-        
+        self.resize(320, 200)
+     
     def resizeEvent(self, event):
-        #print "resize!"
-        while self.thread.isRunning():
-            self.thread.abort = True
-        self.thread = RenderThread(width=self.width(), height=self.height())
+        print "resize!"
+        self.killThread()
+        self.thread = RenderThread(self, width=self.width(), height=self.height())
+        self.connect(self.thread, SIGNAL("newStuff()"), self.update)
         self.thread.start()
 
     def paintEvent(self, event):
         #print "paint!"
         self.thread.mutex.lock()
-        painter = QtGui.QPainter(self)
+        painter = QPainter(self)
         painter.drawPixmap(0, 0, self.thread.pixmap)
         self.thread.mutex.unlock()
 
@@ -93,11 +91,11 @@ class MandelWindow(QtGui.QWidget):
         metrics = painter.fontMetrics()
         textWidth = metrics.width(text)
 
-        painter.setPen(QtCore.Qt.NoPen)
-        painter.setBrush(QtGui.QColor(0, 0, 0, 127))
+        painter.setPen(Qt.NoPen)
+        painter.setBrush(QColor(0, 0, 0, 127))
         painter.drawRect((self.width() - textWidth) / 2 - 5, 0, textWidth + 10,
                          metrics.lineSpacing() + 5)
-        painter.setPen(QtCore.Qt.white)
+        painter.setPen(Qt.white)
         painter.drawText((self.width() - textWidth) / 2,
                          metrics.leading() + metrics.ascent(), text)
 
@@ -105,16 +103,23 @@ class MandelWindow(QtGui.QWidget):
         #print "move!"
         pos = event.pos()
         x,y = self.thread.transfo(pos.x(), pos.y())
-        self.mouse = QtCore.QPointF(x,y)
+        self.mouse = QPointF(x,y)
         self.update()
+    
+    def killThread(self):
+        if self.thread:
+            while self.thread.isRunning():
+                self.thread.abort = True 
+                   
+    def closeEvent(self, event):
+        self.killThread()   # important de ne plus dessiner sinon python.exe plante (mais pas pythonw)
 
 def main():
-    app = QtGui.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     win = MandelWindow()
     win.show()
     app.exec_()
-    #app.connect(app, QtCore.SIGNAL("lastWindowClosed()"),app,QtCore.SLOT("quit()"))
-    #sys.exit(app.exec_())
+    #app.connect(app, SIGNAL("lastWindowClosed()"),app,SLOT("quit()"))
  
 if __name__=="__main__":
     main() 
