ac9c007414bc7578b86a0aeb54ae01cf16603710
Romain Boman <romain.boman@gmail.com>
2016-07-07 09:38:52 +0200

    ajout zoom

--- fractale/mandel.py
+++ fractale/mandel.py
@@ -8,15 +8,18 @@ from PyQt4.QtGui import *
 class RenderThread(QThread):
     def __init__(self, parent, width=100, height=100):
         QThread.__init__(self, parent)
+        
         self.wx = width
         self.wy = height
-        self.inc = 1
+        
+        self.inc = 2
+        
         self.zoom = 300
         self.ox = -0.5
         self.oy = 0.5
-        self.xc = 0.0
-        self.yc = 0.0
+        
         self.nbc = 50
+        
         self.pixmap = QPixmap(width, height)
         self.mutex = QMutex()
         self.abort = False
@@ -26,7 +29,7 @@ class RenderThread(QThread):
         self.mutex.lock()  
         painter = QPainter(self.pixmap)
         painter.fillRect(self.pixmap.rect(), Qt.black)
-        self.mutex.unlock()  
+        self.mutex.unlock()
 
         # setup palette
         colours = []
@@ -37,7 +40,7 @@ class RenderThread(QThread):
         for xe in range(0, self.wx, self.inc):
             for ye in range(0, self.wy, self.inc):
                 #print "xe, ye=" , xe, ye
-                x,y = self.transfo(xe,ye)
+                x,y = self.ax2win(xe,ye)
                 n=0; xn=0.0; yn=0.0
                 while (n!=self.nbc) and (yn*yn+xn*xn<4):
                     n+=1
@@ -53,12 +56,13 @@ class RenderThread(QThread):
 
         #print "thread is over!"
         
-    def transfo(self,xe,ye):
+    def ax2win(self, xe, ye):  # wx,wy,ox,oy,zoom => x,y
         x = self.ox + float(xe-self.wx/2)/self.zoom
         y = self.oy - float(ye-self.wy/2)/self.zoom
-        return x,y         
+        return x, y         
 
-class MandelWindow(QWidget):
+
+class MandelWidget(QWidget):
     """
     Main Qt Widget
     """
@@ -67,10 +71,14 @@ class MandelWindow(QWidget):
         self.setWindowTitle("Mandelbrot (QThread)")
         self.thread = None
 
-        self.mouse = QPoint()
+        self.mouseclick = QPoint()
+        self.mouse = QPoint() 
+        self.showZoom = False
+               
         self.setMouseTracking(True) # appelle mouseMoveEvent meme si pas de clic
         
         self.resize(320, 200)
+
      
     def resizeEvent(self, event):
         print "resize!"
@@ -87,7 +95,8 @@ class MandelWindow(QWidget):
         self.thread.mutex.unlock()
 
         # coords
-        text = "X = %f, Y = %f" % (self.mouse.x(), self.mouse.y())
+        x,y = self.thread.ax2win(self.mouse.x(), self.mouse.y())
+        text = "X = %f, Y = %f" % (x, y)
         metrics = painter.fontMetrics()
         textWidth = metrics.width(text)
 
@@ -99,12 +108,40 @@ class MandelWindow(QWidget):
         painter.drawText((self.width() - textWidth) / 2,
                          metrics.leading() + metrics.ascent(), text)
 
+        # zoom
+        if self.showZoom:
+            rect = QRect(self.mouseclick, self.mouse)
+            painter.setPen(Qt.white)
+            painter.drawRect(rect)            
+
+#    def checkAspect(self, x1,y1, x2,y2):
+#        
+#        arW = float(self.width())/self.height()
+#        w = abs(x2 - x1)
+#        h = abs(y2 - y1)
+#        ar = float(w)/h
+#        if ar>arW:
+            
+            
+        
+        
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            print "left clicked!"
+            self.mouseclick = event.pos()
+            self.showZoom = True
+
     def mouseMoveEvent(self, event):
         #print "move!"
-        pos = event.pos()
-        x,y = self.thread.transfo(pos.x(), pos.y())
-        self.mouse = QPointF(x,y)
+        self.mouse = event.pos()
         self.update()
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            print "left released!"
+            self.showZoom = False
+            #self.mouseclick = event.pos()
     
     def killThread(self):
         if self.thread:
@@ -116,7 +153,7 @@ class MandelWindow(QWidget):
 
 def main():
     app = QApplication(sys.argv)
-    win = MandelWindow()
+    win = MandelWidget()
     win.show()
     app.exec_()
     #app.connect(app, SIGNAL("lastWindowClosed()"),app,SLOT("quit()"))
