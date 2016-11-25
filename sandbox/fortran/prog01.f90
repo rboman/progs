69ea9a58e84d97893c0458079dab7e14a7b5e4fc
Romain Boman <romain.boman@gmail.com>
2016-11-25 18:06:28 +0100

    modifs mineures

--- sandbox/fortran/prog01.f90
+++ sandbox/fortran/prog01.f90
@@ -1,4 +1,19 @@
 program prog01
 
-    print *,'hello world'
+    implicit none
+
+    double precision :: z = 1.0d0 ! <= 'd' car 'e' c'est un real (float) 
+    logical :: flag=.true.
+    real :: b    
+    integer :: i=2, &   ! on peut mettre des commentaires ici
+               n=5 ;    print *,'hello world i=',i ! <= pas depasser la 132e colonne
+    
+    b=2; i=3
+    print *, 'hello &
+          &world coupé', flag, z
+    
+    do i=1,n
+        print *,'i=',i
+    end do
+    
 end
