5fe99316b71530499c216105025beef1dd00d77a
Romain Boman <romain.boman@gmail.com>
2017-04-01 08:51:55 +0200

    compiled vtk example

---
+++ sandbox/vtk/plotwidget/README.md
@@ -0,0 +1,5 @@
+# HistogramXYPlot
+
+Compiled VTK example using vtkXYPlotActor
+
+from [VTK Wiki](http://www.vtk.org/Wiki/VTK/Examples/Cxx/WishList/Images/HistogramXYPlot)
---
+++ sandbox/vtk/plotwidget/build_and_run.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+# -*- coding: latin-1; -*-
+
+progname='HistogramXYPlot'
+
+import os, os.path, sys, subprocess, platform
+
+try:
+    # create build dir
+    if not os.path.isdir('build'): os.mkdir('build')
+    os.chdir('build')
+    # cmake
+    if 'Windows' in platform.uname():
+        subprocess.call('cmake -G "Visual Studio 14 Win64" ..', shell=True)
+    else:
+        subprocess.call('cmake ..', shell=True)
+    subprocess.call('cmake --build . --config Release', shell=True)
+    if 'Windows' in platform.uname(): os.chdir('Release')
+    # run prog
+    #if not os.path.isfile(progname): raise Exception("build failed!")
+    inputf = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'rainbow.jpg')
+    subprocess.check_output([os.path.join('.',progname), inputf], shell=True)
+except Exception as e:
+    print e
+    print "<press ENTER to quit>"; raw_input()
