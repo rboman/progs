ca1c08387ea02212c2aaa1d87001e60d9850a450
Romain Boman <romain.boman@gmail.com>
2017-10-15 11:39:50 +0200

    small improvements

--- sandbox/matplotlib/simple_plot.py
+++ sandbox/matplotlib/simple_plot.py
@@ -1,16 +1,21 @@
 #! /usr/bin/env python
 # -*- coding: latin-1 -*-
 
+# see https://matplotlib.org/tutorials/index.html
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 t = np.arange(0.0, 2.0, 0.01)
 s = np.sin(2*np.pi*t)
-plt.plot(t, s)
+c = np.cos(2*np.pi*t)
+plt.plot(t, s, label='sin')
+plt.plot(t, c, label='cos')
 
 plt.xlabel('time (s)')
 plt.ylabel('voltage (mV)')
 plt.title('About as simple as it gets, folks')
 plt.grid(True)
-plt.savefig("test.png")
+plt.legend()
+#plt.savefig("test.png")
 plt.show()
--- sandbox/matplotlib/test_matplotlib.py
+++ sandbox/matplotlib/test_matplotlib.py
@@ -3,9 +3,8 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import math
 
-x = np.arange(0.0, 2*math.pi, 0.1)
+x = np.arange(0.0, 2*np.pi, 0.1)
 y = np.sin(x)
 plt.plot(x,y, 'bo', x,y,'k')
 plt.xlabel('x')
