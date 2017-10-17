fb99c1852b975fd5a93b1286ccf4a13f39afb73d
Romain Boman <romain.boman@gmail.com>
2017-10-17 15:36:20 +0200

    adding space between slides

--- bin/mkhandout.py
+++ bin/mkhandout.py
@@ -22,14 +22,17 @@ def handout(infile):
     file, ext = os.path.splitext(infile)
     if ext!=".pdf":
         raise Exception("file should be a PDF")
-    outfile=file+"-handout.pdf"
-    cmd='pdfnup --a4paper --keepinfo --nup 1x2 --frame true --scale 0.92 --no-landscape --outfile "%s" "%s"' % (outfile, infile)
+    #outfile=file+"-handout.pdf"
+    #cmd='pdfnup --a4paper --keepinfo --nup 1x2 --frame true --scale 0.92 --no-landscape --outfile "%s" "%s"' % (outfile, infile)
+    cmd=r'pdfnup --a4paper --keepinfo --nup 1x2 --frame true ' \
+         ' --scale 0.92 --no-landscape ' \
+         ' --trim \'-0.2cm -0.2cm -0.2cm -0.2cm\' --delta \'10 10\' ' \
+         ' --suffix 1x2 "%s"' % infile
     print cmd
     os.system(cmd)
 
 if __name__=="__main__":
-    if len(sys.argv)!=2:
-        print "usage: %s file.pdf" % sys.argv[0]
-    else:
-        handout(sys.argv[1])
+    for f in sys.argv[1:]:
+        handout(f)
+
 
