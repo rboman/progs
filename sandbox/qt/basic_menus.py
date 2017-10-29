c1959d1d8c6d755e27338006b02b1e6f3993db97
Romain Boman <romain.boman@gmail.com>
2017-10-29 13:12:18 +0100

    adding tutos from zetcode

---
+++ sandbox/qt/basic_menus.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: latin-1 -*-
+# inspired from http://zetcode.com/gui/pyqt5/
+
+import sys, os
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+
+
+class MenuWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.initUI()
+
+    def initUI(self):
+
+        # actions
+        iconfile = os.path.join(os.path.dirname(__file__),'exit.png')
+        exitAct = QAction(QIcon(iconfile), '&Exit', self)
+        exitAct.setShortcut('Ctrl+Q')
+        exitAct.setStatusTip('Exit application')
+        exitAct.triggered.connect(qApp.quit)
+
+        viewStatAct = QAction('View statusbar', self, checkable=True)
+        viewStatAct.setStatusTip('View statusbar')
+        viewStatAct.setChecked(True)
+        viewStatAct.triggered.connect(self.toggleMenu)
+
+        # status bar
+        self.statusbar = self.statusBar()
+        self.statusbar.showMessage('Ready')
+
+        # menu bar
+        menubar = self.menuBar()
+
+        fileMenu = menubar.addMenu('&File') # simple menu
+        fileMenu.addAction(exitAct)
+
+        impMenu = QMenu('Import', self)  # submenu
+        impAct = QAction('Import mail', self) 
+        impAct.setStatusTip('Import mail')
+        impMenu.addAction(impAct)
+        fileMenu.addMenu(impMenu)
+
+        viewMenu = menubar.addMenu('&View')  # check menu
+        viewMenu.addAction(viewStatAct)
+
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('Simple menus')
+        self.show()
+
+
+    def toggleMenu(self, state):
+        if state:
+            self.statusbar.show()
+        else:
+            self.statusbar.hide()
+
+    def contextMenuEvent(self, event):
+        """add a context menu to the central widget"""   
+        cmenu = QMenu(self)
+        
+        newAct = cmenu.addAction("New")
+        opnAct = cmenu.addAction("Open")
+        quitAct = cmenu.addAction("Quit")
+
+        # display the context menu and wait...
+        action = cmenu.exec_(self.mapToGlobal(event.pos()))
+        
+        # process chosen action
+        if action == quitAct:
+            qApp.quit()
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MenuWindow()
+    sys.exit(app.exec_())
--- sandbox/qt/basic_qapp.py
+++ sandbox/qt/basic_qapp.py
@@ -3,34 +3,52 @@
 # inspired from http://zetcode.com/gui/pyqt5/
 
 import sys
+import os
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 
+"""QMainWindow skeleton
+"""
 
-class Example(QMainWindow):
+
+class MainWindow(QMainWindow):
+
     def __init__(self):
         QMainWindow.__init__(self)
+
         self.initUI()
 
     def initUI(self):
 
-        exitAct = QAction(QIcon('exit.png'), '&Exit', self)
+        # central widget
+        textEdit = QTextEdit()
+        self.setCentralWidget(textEdit)
+
+        # action(s)
+        iconfile = os.path.join(os.path.dirname(__file__), 'exit.png')
+        exitAct = QAction(QIcon(iconfile), 'Exit', self)
         exitAct.setShortcut('Ctrl+Q')
         exitAct.setStatusTip('Exit application')
-        exitAct.triggered.connect(qApp.quit)
+        exitAct.triggered.connect(self.close)
 
+        # status bar
         self.statusBar()
 
+        # menu
         menubar = self.menuBar()
         fileMenu = menubar.addMenu('&File')
         fileMenu.addAction(exitAct)
 
-        self.setGeometry(300, 300, 300, 200)
-        self.setWindowTitle('Simple menu')
+        # toolbar
+        toolbar = self.addToolBar('Exit')
+        toolbar.addAction(exitAct)
+
+        self.setGeometry(300, 300, 350, 250)
+        self.setWindowTitle('Main window')
         self.show()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Example()
+    ex = MainWindow()
     sys.exit(app.exec_())
