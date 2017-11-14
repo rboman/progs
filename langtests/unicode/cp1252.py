fc147d29bf7a89e5fa17ae166b30ec72e98a9d15
Romain Boman <romain.boman@gmail.com>
2017-11-14 15:30:45 +0100

    encoding tests

---
+++ langtests/unicode/cp1252.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# test encoding: à-é-è-ô-ï-€
+
+import sys, os
+
+
+def main():
+    print "sys.stdout.encoding:", sys.stdout.encoding  # cp850 sous windows 10 / UTF-8 sous linux
+    print "sys.getfilesystemencoding():", sys.getfilesystemencoding(
+    )  # mbcs sous windows 10 / UTF-8 sous linux
+
+    fichier = 'cp1252.txt'
+    f = open(fichier)
+    bytes = f.read()
+    f.close()
+    print type(bytes)
+    print bytes
+    txt = bytes.decode(
+        'cp1252')  # le resultat de decode est tjs de l'unicode (utf8)
+    print txt  # print de l'utf8 brutalement - ca marche sous linux pq tout est en utf8...
+    #txtascii = txt.encode('ascii')
+
+
+if __name__ == "__main__":
+    main()
