b66566c1d5ba8a63620a8c07b8f7e12d5773c063
Romain Boman <romain.boman@gmail.com>
2018-03-05 17:01:11 +0100

    test scalapack - marche pas

--- devenv-ifort.bat
+++ devenv-ifort.bat
@@ -17,7 +17,10 @@ set INCLUDE=%MYLOCAL%\include;%MYLOCAL%\MUMPS\include;%MYLOCAL%\eigen-3.3.2
 set LIB=%MYLOCAL%\MUMPS\lib
 
 :: ok (MKL + VS en ligne de commande)
-call "C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\bin\ifortvars.bat" intel64 vs2015
+:: call "C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\bin\ifortvars.bat" intel64 vs2015
 :: call "C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\mkl\bin\mklvars.bat" intel64 vs2015
 :: call "C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\tbb\bin\tbbvars.bat" intel64 vs2015
-%comspec% /K ""C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64"
+
+call "C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\bin\compilervars.bat" intel64 vs2015
+
+%comspec%
