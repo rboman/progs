c188865511dcbd12501c14d19de5fe2d7764e6f1
Romain Boman <romain.boman@gmail.com>
2018-05-17 17:40:50 +0200

    check whether a var is set or not

--- snippets/dash/indirect_ref.sh
+++ snippets/dash/indirect_ref.sh
@@ -1,8 +1,18 @@
 # /usr/bin/env dash
 
 
-for var in PATH LD_LIBRARY_PATH
+for var in PATH LD_LIBRARY_PATH PIF
 do
-   echo " * $var = $(eval echo \$$var)"
+   #echo " * $var = $(eval echo \$$var)"
+   if [ -n "$(eval echo \$$var)" ]
+   then 
+       echo "$var is set"
+       cmd="$var=\$(printf %s \"\$$var\" | awk -v RS=: '{ if (!arr[\$0]++) {printf(\"%s%s\",!ln++?\"\":\":\",\$0)}}')"
+       echo $cmd
+   else 
+       echo "$var is unset" 
+   fi
 done
 
+#cmd=ls
+#eval $(echo $cmd)
