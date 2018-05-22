921fed55d77c97ee71da0e365d525236b5a649d4
Romain Boman <romain.boman@gmail.com>
2018-05-22 15:49:20 +0200

    cleaning

--- sandbox/tbb/thead_local_storage/main.cpp
+++ sandbox/tbb/thead_local_storage/main.cpp
@@ -1,28 +1,28 @@
 // from https://www.threadingbuildingblocks.org/tutorial-intel-tbb-thread-local-storage
 #include <tbb/tbb.h>
-#include <stdio.h>
+#include <cstdio>
 
 using namespace tbb;
 
-typedef tbb::enumerable_thread_specific< std::pair<int,int> > CounterType;
-CounterType MyCounters (std::make_pair(0,0));
+typedef tbb::enumerable_thread_specific<std::pair<int, int>> CounterType;
+CounterType MyCounters(std::make_pair(0, 0));
 
-struct Body 
+struct Body
 {
-void operator()(const tbb::blocked_range<int> &r) const 
-{
-    CounterType::reference my_counter = MyCounters.local();
-    ++my_counter.first;        
-    for (int i = r.begin(); i != r.end(); ++i)            
-        ++my_counter.second;   
-}
+    void operator()(const tbb::blocked_range<int> &r) const
+    {
+        CounterType::reference my_counter = MyCounters.local();
+        ++my_counter.first;
+        for (int i = r.begin(); i != r.end(); ++i)
+            ++my_counter.second;
+    }
 };
 
-int main() 
+int main()
 {
-    tbb::parallel_for( tbb::blocked_range<int>(0, 100000000), Body());
-    
-    for (CounterType::const_iterator i = MyCounters.begin(); i != MyCounters.end();  ++i)
+    tbb::parallel_for(tbb::blocked_range<int>(0, 100000000), Body());
+
+    for (CounterType::const_iterator i = MyCounters.begin(); i != MyCounters.end(); ++i)
     {
         printf("Thread stats:\n");
         printf("  calls to operator(): %d", i->first);
