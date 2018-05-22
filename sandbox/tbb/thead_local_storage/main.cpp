efdaed3448860a74b4af0783dd16160ae5a3f686
Romain Boman <romain.boman@gmail.com>
2018-05-22 15:39:35 +0200

    new test (tbb thread local storage)

---
+++ sandbox/tbb/thead_local_storage/main.cpp
@@ -0,0 +1,31 @@
+// from https://www.threadingbuildingblocks.org/tutorial-intel-tbb-thread-local-storage
+#include <tbb/tbb.h>
+#include <stdio.h>
+
+using namespace tbb;
+
+typedef tbb::enumerable_thread_specific< std::pair<int,int> > CounterType;
+CounterType MyCounters (std::make_pair(0,0));
+
+struct Body 
+{
+void operator()(const tbb::blocked_range<int> &r) const 
+{
+    CounterType::reference my_counter = MyCounters.local();
+    ++my_counter.first;        
+    for (int i = r.begin(); i != r.end(); ++i)            
+        ++my_counter.second;   
+}
+};
+
+int main() 
+{
+    tbb::parallel_for( tbb::blocked_range<int>(0, 100000000), Body());
+    
+    for (CounterType::const_iterator i = MyCounters.begin(); i != MyCounters.end();  ++i)
+    {
+        printf("Thread stats:\n");
+        printf("  calls to operator(): %d", i->first);
+        printf("  total # of iterations executed: %d\n\n", i->second);
+    }
+}
