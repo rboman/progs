4234db90e7437d7994a1f16d6385a1a979df251f
Romain Boman <romain.boman@gmail.com>
2018-07-02 12:15:12 +0200

    improving nas backup script

--- bin/rsync_nas2hdd.sh
+++ bin/rsync_nas2hdd.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
-# backup NAS onto external 4To hdd
+# backup NAS => hdd (USB)
 # to be run on the NAS
 
 echo "***** BACKUP web => hdd"
 
-rsync -avz \
+if [ -d /volumeUSB1/usbshare/web/ ] ; then
+    rsync -avz \
       --delete --delete-excluded \
-	  --exclude=/robots.txt \
+	--exclude=/robots.txt \
       --exclude=cache/ \
       --exclude=locks/ \
 	  /volume1/web/ \
 	  /volumeUSB1/usbshare/web/
+else
+    echo "ERROR: /volumeUSB1/usbshare/web/ not present!"
+    echo "       /volumeUSB1/usbshare/ not mounted?"
+fi
+
+# ---
 
 echo
 echo "***** BACKUP ftp => hdd"
 
-rsync -avz \
+if [ -d /volumeUSB1/usbshare/ftp/ ] ; then
+    rsync -avz \
       --delete --delete-excluded \
       --exclude="@eaDir" \
       /volume1/ftp/ \
       /volumeUSB1/usbshare/ftp/
+else
+    echo "ERROR: /volumeUSB1/usbshare/ftp/ not present!"
+    echo "       /volumeUSB1/usbshare/ not mounted?"
+fi
 
