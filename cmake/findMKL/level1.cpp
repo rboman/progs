c8a5a55b8f21a90978505d7c609aba1fa3cd0615
Romain Boman <romain.boman@gmail.com>
2019-04-13 18:25:06 +0200

    clean build system

--- cmake/findMKL/level1.cpp
+++ cmake/findMKL/level1.cpp
@@ -1,16 +1,16 @@
-
 #include <mkl.h>
 #include <iostream>
 #include <vector>
 #include <cassert>
 
-std::ostream &operator <<(std::ostream &out, std::vector<double> const &v)
+std::ostream &operator<<(std::ostream &out, std::vector<double> const &v)
 {
     out << '[';
-    for(auto it=v.begin(); it!=v.end(); ++it)
+    for (auto it = v.begin(); it != v.end(); ++it)
     {
         out << *it;
-        if(it+1!=v.end()) out << ", ";
+        if (it + 1 != v.end())
+            out << ", ";
     }
     out << ']';
     return out;
@@ -19,18 +19,18 @@ std::ostream &operator <<(std::ostream &out, std::vector<double> const &v)
 std::vector<double>
 mydaxpy(double a, std::vector<double> const &x, std::vector<double> const &y)
 {
-    assert(x.size()==y.size());
+    assert(x.size() == y.size());
     auto n = x.size();
     std::vector<double> res(n);
-    for(size_t i=0; i<n; ++i)
-        res[i] = a*x[i]+y[i];
+    for (size_t i = 0; i < n; ++i)
+        res[i] = a * x[i] + y[i];
     return res;
 }
 
 int main(int argc, char **argv)
 {
-    std::vector<double> x { 2.0, 1.0, -1.0 };
-    std::vector<double> y { 1.0, 2.0, 3.0 };
+    std::vector<double> x{2.0, 1.0, -1.0};
+    std::vector<double> y{1.0, 2.0, 3.0};
     MKL_INT n = (MKL_INT)x.size();
 
     double a = 2.0;
@@ -44,9 +44,8 @@ int main(int argc, char **argv)
     auto y2 = mydaxpy(a, x, y);
     std::cout << "mydaxpy=" << y2 << '\n';
 
-    cblas_daxpy (n, a, &(x[0]), incx, &(y[0]), incy);
+    cblas_daxpy(n, a, &(x[0]), incx, &(y[0]), incy);
     std::cout << "daxpy=" << y << '\n';
 
     return 0;
 }
-
