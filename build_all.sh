19310f739840d2930c14be5f0d11f830927e3127
Romain Boman <romain.boman@gmail.com>
2019-04-14 07:57:05 +0200

    finger crossed

--- build_all.sh
+++ build_all.sh
@@ -3,7 +3,7 @@
 PROG_BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd ${PROG_BASE_DIR}/apps/EHD  && ./travis.sh
-cd ${PROG_BASE_DIR}/apps/cpp/fractal  && ./travis.sh
+cd ${PROG_BASE_DIR}/apps/fractal/cpp  && ./travis.sh
 cd ${PROG_BASE_DIR}/apps/GenMAI  && ./travis.sh
 cd ${PROG_BASE_DIR}/apps/md5  && ./travis.sh
 cd ${PROG_BASE_DIR}/apps/minibarreTE  && ./travis.sh    # requires gmm
