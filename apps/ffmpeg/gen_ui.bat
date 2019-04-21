c85bea41a36c6d716301a1c3744a55d8147dd7f2
Romain Boman <romain.boman@gmail.com>
2019-04-21 19:27:17 +0200

    portable gen_ui

--- apps/ffmpeg/gen_ui.bat
+++
@@ -1,1 +0,0 @@
-f:\local\python-2.7.12\pyuic5.bat widget.ui -o ui_widget.py
---
+++ apps/ffmpeg/gen_ui.py
@@ -0,0 +1,65 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Execute "pyuic". 
+# Ask for the location of it if not found.
+# Store result in QSettings
+
+import sys
+import subprocess
+import os
+import platform
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+def tryEXE(exe):
+    try:
+        print "dry run of '%s'..." % exe,
+        with open(os.devnull, 'w') as FNULL:
+            subprocess.call([exe], stdout=FNULL, stderr=subprocess.STDOUT)
+        print "OK!"
+        return exe
+    except OSError:
+        print "failure!"
+        return ""
+
+def askUser():
+    app = QApplication(sys.argv)
+    ext = '.*' if 'Windows' in platform.uname() else ''
+    fname = QFileDialog.getOpenFileName(
+        None, 'Select pyuic5', '', filter='Executable (pyuic5%s)' % ext)
+    exe = fname[0]
+    if not exe:
+        print 'cancelled by user'
+        sys.exit(1)
+    print "user selected '%s'" % exe
+    return exe
+
+if __name__ == "__main__":
+
+    exe = 'pyuic5'
+    if tryEXE(exe):  # try exe
+        pass
+    else:
+        # read settings
+        settings = QSettings("rboman", "progs")
+        #settings.remove("pyuic5") # clear stored value
+
+        exe = settings.value("pyuic5", "")
+        print "using value stored in settings: '%s'" % exe
+
+        if exe and tryEXE(exe):
+            pass
+        else:
+            exe = askUser()
+            if tryEXE(exe):
+                settings.setValue("pyuic5", QVariant(exe))
+            else:
+                raise Exception("'%s' does not work!" % exe)
+
+    # run cmd
+    cmd = [exe, 'widget.ui', '-o', 'ui_widget.py']
+    print "=> running", ' '.join(cmd)
+    iop = subprocess.call(cmd)
+    print 'iop =', iop
