b62a4db7b052776bd67d90213f776397e3e50995
Romain Boman <romain.boman@gmail.com>
2019-04-21 19:26:53 +0200

    more compact indent + doc

--- build_all.py
+++ build_all.py
@@ -4,43 +4,22 @@
 import os
 import subprocess
 
+# list of projects
 progs = [
-    {
-        'path': 'apps/EHD',
-        'travis': True
-    },
-    {
-        'path': 'apps/fractal/cpp',
-        'travis': False
-    },
-    {
-        'path': 'apps/GenMAI',
-        'travis': True
-    },
-    {
-        'path': 'apps/md5',
-        'travis': True
-    },
-    {
-        'path': 'apps/minibarreTE',
-        'travis': True
-    },
-    {
-        'path': 'student/dcm1',
-        'travis': False
-    },
-    {
-        'path': 'student/dcm2',
-        'travis': True
-    },
-    {
-        'path': 'student/ndh',
-        'travis': True
-    }
+    {'path': 'apps/EHD', 'travis': True},
+    {'path': 'apps/fractal/cpp', 'travis': False},
+    {'path': 'apps/GenMAI', 'travis': True},
+    {'path': 'apps/md5', 'travis': True},
+    {'path': 'apps/minibarreTE', 'travis': True},
+    {'path': 'student/dcm1', 'travis': False},
+    {'path': 'student/dcm2', 'travis': True},
+    {'path': 'student/ndh', 'travis': True}
 ]
 
 
 def build_one(basedir, p):
+    """build project 'p'
+    """
     fullpath = os.path.join(basedir, *(p['path'].split('/')))
     if(p['travis']):
         print '=> running build.py in', fullpath
@@ -49,15 +28,18 @@ def build_one(basedir, p):
 
 
 def build_all(basedir):
+    """build everything in 'basedir'
+    """
     for p in progs:
         build_one(basedir, p)
 
 
 def rm_builds(basedir):
+    """remove all 'build' directories in 'basedir'
+    """
     import shutil
     for path, subdirs, files in os.walk(basedir):
         for name in subdirs:
-            # print name
             if name == 'build':
                 fullname = os.path.join(path, name)
                 print 'removing', fullname
