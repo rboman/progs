05c298bf3a99cf1914afd91d88d9a20fc01535b5
Romain Boman <romain.boman@gmail.com>
2019-05-18 17:04:24 +0200

    msys2 fortran compiler

---
+++ devenv-msys2-mingw64.bat
@@ -0,0 +1,19 @@
+@echo off
+
+echo setting msys64\mingw64 environment
+
+set INCLUDE=
+::set INCLUDE=%INCLUDE%;C:\local\MUMPS\include
+
+set LIB=
+::set LIB=c:\local\lib
+
+set OMP_NUM_THREADS=1
+
+set PATH=C:\msys64\mingw64\bin
+::set PATH=%PATH%;C:\MinGW\msys\1.0\bin
+set PATH=%PATH%;C:\Program Files\CMake\bin\
+set PATH=%PATH%;C:\Program Files\Microsoft MPI\Bin\
+
+
+%comspec%
