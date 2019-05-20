eb90e94f77195c92f5ce4dcdcb7826e09698d391
Romain Boman <romain.boman@gmail.com>
2019-05-20 13:48:38 +0200

    try to separate files from eispack and sparskit

--- student/tfe/common/sparfun.f90
+++ student/tfe/common/sparfun.f90
@@ -1,5 +1,5 @@
 !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-! Sous-routines & fonctions matricielles élémentaires
+! Sous-routines & fonctions matricielles Ã©lÃ©mentaires
 ! (Version SPARSE MATRIX)
 ! 17.11.96
 !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -9,58 +9,61 @@
 
 SUBROUTINE SMMV(N, A, IA, JA, b, c)
     implicit none
-    INTEGER :: N, i, j, IA(*), JA(*)
-    REAL*8 :: A(*), b(*), c(*), t
+    INTEGER, intent(in) :: N, IA(*), JA(*)
+    REAL*8, intent(in) :: A(*), b(*)
+    REAL*8, intent(out) :: c(*)
 
-    DO i=1,N
-        t=0.0d0
-        DO j=IA(i), IA(i+1)-1
-            t=t+A(j)*b(JA(j))
+    REAL*8 :: t
+    INTEGER :: i, j
+
+    DO i = 1, N
+        t = 0.0d0
+        DO j = IA(i), IA(i+1)-1
+            t = t + A(j) * b(JA(j))
         END DO
         c(i)=t
     END DO
-    RETURN
 END SUBROUTINE SMMV
 
 ! Calcule c = A^T * b    (sparse matrix - CSR format)
 
 SUBROUTINE SMMVT(N, A, IA, JA, b, c)
     implicit none
-    INTEGER :: N, i, j, k, IA(*), JA(*)
-    REAL*8 :: A(*), b(*), c(*)
+    INTEGER, intent(in) :: N, IA(*), JA(*)
+    REAL*8, intent(in) :: A(*), b(*)
+    REAL*8, intent(out) :: c(*)
+    INTEGER :: i, j, k
 
-    DO i=1,N
-    c(i)=0.0D0
+    DO i = 1, N
+        c(i) = 0.0D0
     ENDDO
 
-    DO i=1,N
-        DO j=IA(i), IA(i+1)-1
-            k=JA(j)
-            c(k)=c(k)+A(j)*b(i)
+    DO i = 1, N
+        DO j = IA(i), IA(i+1)-1
+            k = JA(j)
+            c(k) = c(k) + A(j) * b(i)
         END DO
     END DO
-    RETURN
 END SUBROUTINE SMMVT
 
-! Fournit H(i,j)   (où H est triu + diag)
+! Fournit H(i,j)   (oÃ¹ H est triu + diag)
 
-REAL*8 FUNCTION HCALL(H,i,j)
+REAL*8 FUNCTION HCALL(H, i, j)
     implicit none
-    REAL*8 :: H(*)
-    INTEGER :: i,j
+    REAL*8, intent(in) :: H(*)
+    INTEGER, intent(in) :: i,j
 
-    HCALL=H((j-1)*(j+2)/2+i)
-    RETURN
+    HCALL = H( (j-1)*(j+2)/2 + i )
 END FUNCTION HCALL
 
 
-! Effectue H(i,j):=alpha  (où H est triu + diag)
+! Effectue H(i,j):=alpha  (oÃ¹ H est triu + diag)
 
-SUBROUTINE HASSIGN(H,i,j,alpha)
+SUBROUTINE HASSIGN(H, i, j, alpha)
     implicit none
-    REAL*8 :: H(*), alpha
-    INTEGER :: i,j
+    REAL*8, intent(out) :: H(*)
+    REAL*8, intent(in)  :: alpha
+    INTEGER, intent(in) :: i, j
     
-    H(((j-1)*(j+2))/2+i) = alpha
-    RETURN
+    H( ((j-1)*(j+2))/2 + i ) = alpha
 END SUBROUTINE HASSIGN
