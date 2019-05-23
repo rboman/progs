0df05dea61edba48ecf22c5556806a7415d6bcfb
Romain Boman <romain.boman@gmail.com>
2019-05-23 09:23:17 +0200

    try to guess where ffmpeg is

--- bin/avi2mp4.py
+++ bin/avi2mp4.py
@@ -6,12 +6,41 @@
 
 import sys, os, subprocess, glob
 
-exeffmpeg = r'f:\local\ffmpeg\bin\ffmpeg.exe'
+#exeffmpeg = r'f:\local\ffmpeg\bin\ffmpeg.exe'
 fps=30
 quality= 18
 """
 f:\local\ffmpeg\bin\ffmpeg.exe -i animation_trapez.avi -vf fps=30 -crf 18 -pix_fmt yuv420p video.mp4
 """
+
+def checkExe(exe):
+    try:
+        # try to call it (with a dummy arg - faster than -h)
+        with open(os.devnull, 'w') as FNULL:
+            subprocess.call([exe,'-prout'], stdout=FNULL, stderr=subprocess.STDOUT)
+        return exe
+    except OSError:
+        return ""
+
+def getExe(exe):
+
+    exes = ['ffmpeg']
+    
+    # try to check registry
+    if 'MYLOCAL' in os.environ:
+        mylocal = os.environ['MYLOCAL']
+        exes.append( os.path.join(mylocal,'ffmpeg','bin','ffmpeg') )
+
+    for exe in exes:
+        print 'testing %s' % exe,
+        found = checkExe(exe)
+        if found:
+            print 'OK!'
+            return found
+        else:
+            print 'not found'
+    return ""
+
 
 def convert(f):
     name,ext = os.path.splitext(f)
@@ -19,6 +48,10 @@ def convert(f):
     #    print 'ignoring %s (not .avi)' % f
     #    return
     
+    exeffmpeg = getExe('ffmpeg')
+    if not exeffmpeg:
+        raise Exception("ffmpeg not found!")
+
     cmd = []
     cmd.append(exeffmpeg)    
     cmd.append('-y')
