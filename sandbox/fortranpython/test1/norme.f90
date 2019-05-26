42068f6649efe9d17f89fd54b2e1b937e7fd31f6
Romain Boman <romain.boman@gmail.com>
2019-05-26 08:02:05 +0200

    cmake+f2py (wip)

--- sandbox/fortranpython/test1/norme.f90
+++ sandbox/fortranpython/test1/norme.f90
@@ -4,8 +4,13 @@
 ! f2py2 norme.f90 -m vect -h norme.pyf
 !   produit norme.pyf, un fichier de signature
 !
-! compile direct avec gfortran:
+! compile direct avec gfortran (=>vect.pyd):
 ! f2py2 -c norme.f90 -m vect --fcompiler=gnu95
+
+
+! depends on
+!C:\msys64\mingw64\lib\python2.7\site-packages/numpy/f2py\src\fortranobject.c
+!C:\msys64\mingw64\lib\python2.7\site-packages/numpy/f2py\src\fortranobject.h
 
 subroutine norme ( a , b )
     ! Calcule la norme
