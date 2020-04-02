75630f673f0f1f1f57a630c30275df144ade9fc7
Romain Boman <romain.boman@gmail.com>
2020-04-02 11:40:28 +0200

    test github api

---
+++ sandbox/webapi/githubapi_readtoken.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+import requests
+
+def get_api_token():
+    """read my api token
+    """
+    with open('E:\Dropbox\Bin\github_api_token.txt') as f:
+        token = f.readline().rstrip()
+    print "token='{}'".format(token)
+    return token
+
+def get_all_projects():
+    """get all the projects visible by me on github.com
+    """
+    url = 'https://api.github.com/users/rboman/repos'
+    #url = 'https://api.github.com/orgs/ulgltas/repos'
+    token = get_api_token()
+
+    r = requests.get(url, headers={ "Authorization": 'token {}'.format(token) }) #, params={'per_page' : 1000, 'page':1 })
+
+    print 'r.status_code =', r.status_code
+    # print 'r.headers =', r.headers
+    # print 'r.encoding =', r.encoding
+    # print 'r.url =', r.url
+    # print 'r.text =', r.text
+    # print 'r.json() =', r.json()
+    projects = r.json()
+    return projects
+
+projects = get_all_projects()
+
+# save projects to file
+with open('projects.json','w') as f:
+    f.write( json.dumps(projects, sort_keys=True, indent=4) )
+
+
+# print one project
+#print 'r.json() ='
+#print(json.dumps(projects[0], sort_keys=True, indent=4))
+
+# list projects
+for i,p in enumerate(projects):
+    print "%03d %s (id=%d)" % (i, p["name"], p["id"])
+
