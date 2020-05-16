2ff5bdf66bef06d4ab897aded80a3add51ca6271
Romain Boman <romain.boman@gmail.com>
2020-05-16 15:25:48 +0200

    add script

---
+++ sandbox/python/whichpython.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+import subprocess
+import sys
+import re
+
+pyexe=sys.executable
+
+out = subprocess.check_output([pyexe, '-c', 
+    'from __future__ import print_function; import sys; print(sys.version_info.major)'])
+try:
+    pyver = int(out)
+except:
+    print('unable to get python version number')
+print('Python version is', pyver)
