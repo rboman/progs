3b9f5c610caa1407bc3e12f8272b710c2eadb88b
Romain Boman <romain.boman@gmail.com>
2020-05-21 09:48:21 +0200

    ajout notes

--- sandbox/swig-leak/test_leak.py
+++ sandbox/swig-leak/test_leak.py
@@ -9,7 +9,7 @@ pth = os.path.join('build-py%d'%sys.version_info.major,'bin')
 if 'Windows' in platform.uname():
     pth = os.path.join(pth,'Release') 
 sys.path.append(pth)
-print(sys.path)
+# print(sys.path)
 del pth
 
 import leak 
@@ -42,10 +42,40 @@ d=lambda x: x   # leak with py3!
 # b=1.0               # ok
 # b=1                 # ok
 # b=[]                # ok
-#a = leak.ObjA(c)      # leak with py3! even b is not deleted!
-a = leak.ObjA(d)     # leak with py3! b,c not deleted
+#a = leak.ObjA(c)     # leak with py3! even b is not deleted!
+#a = leak.ObjA(d)      # leak with py3! no global object of this module are deleted!
+a = leak.ObjA(lambda x: x)     # marche pas mieux
 #a = ObjA2(c)
-# del a
+
+
+#del a            # appel explicite à "del a" => OK
+# probleme: en python3, "del a" n'est pas appelé lors de l'arrêt de l'interpréteur
+#           en python2, "del a" est appelé
+
+# ------------------------------------------------------------------------------
+# dans Metafor, 
+#    - definir un objet de type leak.ObjA dans l'espace global de Metafor
+#    - definir un objet global qui pointe vers leak.ObjA (variable metafor)
+# ...empeche la destruction du module
+# 
+# remarque: si leak.ObjA est créé puis détruit dans le module, sans être "gardé" par le module
+#           soit directement a travers une variable globale soit indirectement 
+#           (une de ses variables globales - metafor - fait référence au leak.ObjA),
+#           ca marche et le module appelle les destructeurs de ses variables globales en sortie.
+#
+# resumé: les variables globales d'un module qui a créé un leak.ObjA qui est resté référencé jusqu'à la fin de
+#         l'interpréteur ne seront pas détruites par le PyFinalise de python3.
+#         toutes les autres variables de ce module (y compris ce leak.ObjA s'il n'est pas global a ce module 
+#         sont détruites correctement!)
+#
+# solutions:
+#    ne pas utiliser de "global metafor" 
+#             => metafor est détruit correctement
+#             => seules les variables globales du module ne sont pas détruites
+#             => si leak.ObjA n'est pas global au module, il sera détruit également mais toujours pas les vars globales du module!
+# ------------------------------------------------------------------------------
+
+
 #print (gc.garbage)
 #gc.set_debug(gc.DEBUG_LEAK)
 #gc.collect()
