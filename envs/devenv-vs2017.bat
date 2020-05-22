3412fd5dbf75dd136ed613312a330dc408372e89
Romain Boman <romain.boman@gmail.com>
2020-05-22 18:25:23 +0200

    try to port f2py to visual studio (to be continued)

--- envs/devenv-vs2017.bat
+++ envs/devenv-vs2017.bat
@@ -16,7 +16,10 @@ set PATH=%PATH%;%MYLOCAL%\swig
 set INCLUDE=%MYLOCAL%\include;%MYLOCAL%\MUMPS\include;%MYLOCAL%\eigen
 set LIB=%MYLOCAL%\MUMPS\lib
 
-set PATH=%PATH%;C:\Program Files\Git\bin
+::set PATH=%PATH%;C:\Program Files\Git\bin
+:: f2py
+set PATH=%PATH%;F:\Python37\Scripts
+
 
 :: ok (MKL + VS en ligne de commande)
 call "C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\mkl\bin\mklvars.bat" intel64 vs2017
