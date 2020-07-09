bb59eeb5bb23ff3fcd6bc37a3e4d9e5b72f6cd86
Romain Boman <romain.boman@gmail.com>
2020-07-09 10:05:23 +0200

    add f string to avoid python2 runs

--- bin/rb.py
+++ bin/rb.py
@@ -18,6 +18,7 @@
 
 from past.builtins import execfile
 
+
 def exec_pyfile(fname, searchdir, args, banner=False):
 
     if os.path.isfile(os.path.abspath(fname)):
@@ -29,7 +30,8 @@ def exec_pyfile(fname, searchdir, args, banner=False):
 
     # start test
     if banner:
-        import time, platform
+        import time
+        import platform
         print('-' * 79)
         print("starting test", testname)
         print("time:", time.strftime("%c"))
@@ -39,7 +41,7 @@ def exec_pyfile(fname, searchdir, args, banner=False):
     # setup arguments
     sys.argv = [fname]
     sys.argv.extend(args)
-    
+
     env = globals()
     env['__file__'] = testname
 
@@ -47,24 +49,29 @@ def exec_pyfile(fname, searchdir, args, banner=False):
 
 
 if __name__ == "__main__":
-    import sys, os, os.path
+    import sys
+    import os
+    import os.path
 
     # process arguments
     import argparse
-    parser = argparse.ArgumentParser(description='Script launcher') #, add_help=False)
+    parser = argparse.ArgumentParser(
+        description='Script launcher')  # , add_help=False)
     parser.add_argument('script', help='python script', metavar='script.py')
-    parser.add_argument('--banner', help='display info banner', action="store_true")
-    parser.add_argument( "-v", "--verb", help="increase output verbosity", action="store_true")
+    parser.add_argument(
+        '--banner', help='display info banner', action="store_true")
+    parser.add_argument(
+        "-v", "--verb", help="increase output verbosity", action="store_true")
     args, otherargs = parser.parse_known_args()
-    #print("args={}".format(args))
-    #print("otherargs={}".format(otherargs))  # given to the script
+    # print("args={}".format(args))
+    # print("otherargs={}".format(otherargs))  # given to the script
 
     # How to call --help from the script if add_help=True?
     #    rb.py script.py --help     => display help from rb.py!
     #    rb.py --help               => idem
     #    rb.py -- script.py --help  => display help from script.py
     # Doc:
-    #    pseudo-argument '--' tells parse_args() that everything after 
+    #    pseudo-argument '--' tells parse_args() that everything after
     #    that is a positional argument
 
     # adds ".." to the pythonpath
@@ -73,10 +80,10 @@ if __name__ == "__main__":
     #print("adding '%s' to PYTHONPATH" % parentdir)
     sys.path.append(parentdir)
 
-    scriptdir = os.path.join(thisdir, 'scripts') # could be extended to a list
+    scriptdir = os.path.join(thisdir, 'scripts')  # could be extended to a list
 
     (root, ext) = os.path.splitext(args.script)
     if ext.lower() == '.py':
         exec_pyfile(args.script, scriptdir, otherargs, args.banner)
     else:
-        print("I don't know what to do with '%s'" % args.script)
+        print(f"I don't know what to do with {args.script}")
