a03dc8849edaa326c953673573cdea490e492a1e
Romain Boman <romain.boman@gmail.com>
2020-07-27 18:19:16 +0200

    basic hello world in c#

---
+++ sandbox/csharp/tests/HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿// from C# crash course
+// build cmdline:
+//      c:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe HelloWorld.cs
+//      => produces "HelloWorld.exe"
+
+class HelloCSharp
+{
+    static void Main()
+    {
+        System.Console.WriteLine("Hello C#!");
+    }
+}
