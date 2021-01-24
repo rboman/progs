074131710aad5068897a34679c1fae2689aa3dff
Romain Boman <r.boman@uliege.be>
2021-01-24 18:17:28 +0100

    play with sounds and events

--- sandbox/pygame/events.py
+++ sandbox/pygame/events.py
@@ -8,62 +8,106 @@
 import sys
 import time
 import pygame
-from pygame.locals import * # imports K_LEFT, K_RIGHT into the main namespace
+from pygame.locals import *  # imports K_LEFT, K_RIGHT into the main namespace
 
-# print(dir())
+# print(dir()) # lots of things!
+
 
 def main():
     pygame.init()
 
-    screen = pygame.display.set_mode( (800, 600) )
+    screen = pygame.display.set_mode((800, 600), RESIZABLE)
 
-    ball = pygame.image.load("ball.gif")
-    ballrect = ball.get_rect()
+    ball = pygame.image.load("ball.gif").convert_alpha()
+
+    # pygame.display.set_icon(ball) # marche pas
+
+    # the position of the ball is tracked with a Rect
+    ballrect = ball.get_rect() # initialised as the Rect of the image
+
+    ballspeed = 3
 
     speed = [0, 0]
     kpressed = {}
 
     # game loop
     clock = pygame.time.Clock()
+    
+    pygame.event.set_blocked(MOUSEMOTION) # don't capture MouseMotion
+
     while 1:
 
-        # loop type 1 => holding a key does not produce any event
-        for event in pygame.event.get():
-            print(f"{pygame.event.event_name(event.type)} ({event.type})")
-            if event.type == pygame.QUIT:
-                sys.exit()
-            elif event.type == KEYDOWN:
-                # print('KEYDOWN')
-                if event.key == K_q or event.key == K_LEFT:
-                    speed[0] = -1
-                    print("left")
-                if event.key == K_d or event.key == K_RIGHT:
-                    speed[0] = 1
-                    print("right")
-                if event.key == K_z or event.key == K_UP:
-                    speed[1] = -1
-                    print("up")
-                if event.key == K_s or event.key == K_DOWN:
-                    speed[1] = 1
-                    print("down")
-            elif event.type == KEYUP:
-                # print('KEYUP')
-                if event.key in [ K_q, K_d, K_LEFT, K_RIGHT]:
-                    speed[0] = 0
-                    print("stop x")
-                if event.key in [ K_s, K_z, K_UP, K_DOWN]:
-                    speed[1] = 0
-                    print("stop y")
+        # loop type 1 => holding an arrow key does not produce any event 
+        #                (unless pygame.key.set_repeat is set)
+        #             => a key produces TextEditing event
 
-        ballrect = ballrect.move(speed)
+        if 0:
+            for event in pygame.event.get():
+                print(f"{pygame.event.event_name(event.type)} ({event.type})")
+                if event.type == pygame.QUIT:
+                    return
+                elif event.type == KEYDOWN:
+                    # print('KEYDOWN')
+                    if event.key == K_q or event.key == K_LEFT:
+                        speed[0] = -ballspeed
+                        print("left")
+                    if event.key == K_d or event.key == K_RIGHT:
+                        speed[0] = ballspeed
+                        print("right")
+                    if event.key == K_z or event.key == K_UP:
+                        speed[1] = -ballspeed
+                        print("up")
+                    if event.key == K_s or event.key == K_DOWN:
+                        speed[1] = ballspeed
+                        print("down")
+                elif event.type == KEYUP:
+                    # print('KEYUP')
+                    if event.key in [K_q, K_d, K_LEFT, K_RIGHT]:
+                        speed[0] = 0
+                        print("stop x")
+                    if event.key in [K_s, K_z, K_UP, K_DOWN]:
+                        speed[1] = 0
+                        print("stop y")
+        else:  # loop type 2: we keep the pressed state in a dict
+            for event in pygame.event.get():
+                print(f"{pygame.event.event_name(event.type)} ({event.type})")
+                if event.type == pygame.QUIT:
+                    return
+                elif event.type == KEYDOWN:
+                    kpressed[event.key] = True
+                elif event.type == KEYUP:
+                    kpressed[event.key] = False
 
-        screen.fill( (0,0,0) )
+            # set the velocity of the ball along X
+            if kpressed.get(K_q) or kpressed.get(K_LEFT):
+                speed[0] = -ballspeed
+            elif kpressed.get(K_d) or kpressed.get(K_RIGHT):
+                speed[0] = ballspeed
+            else:
+                speed[0] = 0
+
+            # set the velocity of the ball along Y
+            if kpressed.get(K_z) or kpressed.get(K_UP):
+                speed[1] = -ballspeed
+            elif kpressed.get(K_s) or kpressed.get(K_DOWN):
+                speed[1] = ballspeed
+            else:
+                speed[1] = 0
+
+        # ballrect = ballrect.move(speed) # move the rect
+        ballrect.move_ip(speed) # move the rect "in place"
+        ballrect.clamp_ip(screen.get_rect()) # clamp "in place" the ball rect inside the screen rect.
+
+        # clears the screen
+        screen.fill((0, 0, 50))
+
         screen.blit(ball, ballrect)
-        pygame.draw.rect(screen, (255,255,255), ballrect, 1) # display bounding rect
-        #time.sleep(0.01)
+        pygame.draw.rect(screen, (255, 255, 255), ballrect,
+                         1)  # display the bounding rect
+
+        # time.sleep(0.01)
         pygame.display.flip()
         clock.tick(60)
-
 
 
 if __name__ == "__main__":
--- sandbox/pygame/tutorial1_and_music.py
+++ sandbox/pygame/tutorial1_and_music.py
@@ -2,15 +2,26 @@
 # -*- coding: utf-8 -*-
 
 # from https://fr.wikibooks.org/wiki/Pygame/Introduction_%C3%A0_Pygame
+# and https://nerdparadise.com/programming/pygame/part3
+
+# music from Patrick de Arteaga: 
+#   https://patrickdearteaga.com/chiptune-8-bit-retro/
+# bounce sound from https://freesound.org/people/bumpelsnake/sounds/456567/
+
 
 import sys, time, pygame
 pygame.init()
 
-size = width, height = 320, 240
-speed = [2, 2]
+size = width, height = 640, 480
+speed = [4, 4]
 black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
+
+pygame.mixer.music.load('Common_Fight.ogg')
+pygame.mixer.music.play(-1) # 0= play once; -1: infinite
+effect = pygame.mixer.Sound('bounce.ogg')
+effect.set_volume(0.4)  # between 0 and 1
 
 ball = pygame.image.load("ball.gif")
 ballrect = ball.get_rect()
@@ -22,9 +33,10 @@ while 1:
     ballrect = ballrect.move(speed)
     if ballrect.left < 0 or ballrect.right > width:
         speed[0] = -speed[0]
+        effect.play()
     if ballrect.top < 0 or ballrect.bottom > height:
         speed[1] = -speed[1]
-
+        effect.play()
     screen.fill(black)
     screen.blit(ball, ballrect)
     time.sleep(0.01)
