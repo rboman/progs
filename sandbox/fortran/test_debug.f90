fa380d65dc2416bf37605c8f202fcdc4ab61b40a
Romain Boman <r.boman@uliege.be>
2021-02-20 18:14:02 +0100

    add debug example in vscode

---
+++ sandbox/fortran/test_debug.f90
@@ -0,0 +1,26 @@
+!
+!
+!
+!
+!
+
+program test_debug
+
+
+    type s_bandecage
+        logical :: etatok
+        logical :: selection
+    end type s_bandecage
+
+    type(s_bandecage)         :: BandeCage
+
+    BandeCage%etatok = .true.
+    BandeCage%selection = .false.
+    
+    if( BandeCage%etatok .eqv. .true. ) then
+        print *, "BandeCage%etaok == .true."
+    else
+        print *, "BandeCage%etaok == .false."
+    end if
+
+end program test_debug
