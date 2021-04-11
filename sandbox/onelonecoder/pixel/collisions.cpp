713113cb94f65094fc9cb6fbaa04307036956c65
Romain Boman <r.boman@uliege.be>
2021-04-11 18:08:36 +0200

    collisions

--- sandbox/onelonecoder/pixel/collisions.cpp
+++ sandbox/onelonecoder/pixel/collisions.cpp
@@ -3,6 +3,10 @@
 // videos:
 //  Programming Balls #1 Circle Vs Circle Collisions
 //      https://www.youtube.com/watch?v=LPzyNOHY3A4&t=1s&ab_channel=javidx9
+//  Elastic collisions:
+//      https://en.wikipedia.org/wiki/Elastic_collision
+//
+// Future:
 //  Convex Polygon Collisions #1
 //      https://www.youtube.com/watch?v=7Ik2vowGcU0&ab_channel=javidx9
 //  Circle Vs Rectangle Collisions
@@ -20,6 +24,7 @@ struct Ball
     olc::vf2d velocity;
     olc::vf2d accel;
     float radius;
+    float mass;
     int id;
 };
 
@@ -39,6 +44,7 @@ private:
         b.velocity = {0.0f, 0.0f};
         b.accel = {0.0f, 0.0f};
         b.radius = r;
+        b.mass = r * 10.0f;
         b.id = balls.size();
         balls.push_back(b);
     }
@@ -50,7 +56,7 @@ private:
         addBall(ScreenWidth() * 0.75f, ScreenHeight() * 0.5f, defRadius);
 
         for (int i = 0; i < 10; ++i)
-            addBall(rand() % ScreenWidth(), rand() % ScreenHeight(), defRadius);
+            addBall(rand() % ScreenWidth(), rand() % ScreenHeight(), rand() % 16 + 2);
 
         return true;
     }
@@ -65,8 +71,8 @@ private:
             return (c1.x - c2.x) * (c1.x - c2.x) + (c1.y - c2.y) * (c1.y - c2.y) < (r1) * (r1);
         };
 
-        // mouse interaction
-        if (GetMouse(0).bPressed)
+        // mouse interaction - pick a ball with the mouse and move it
+        if (GetMouse(0).bPressed || GetMouse(1).bPressed)
         {
             selectedBall = nullptr;
             for (auto &ball : balls)
@@ -91,8 +97,46 @@ private:
         {
             selectedBall = nullptr;
         }
+        if (GetMouse(1).bReleased)
+        {
+            if (selectedBall != nullptr)
+            {
+                selectedBall->velocity.x = -5.0f * (float(GetMouseX()) - selectedBall->centre.x);
+                selectedBall->velocity.y = -5.0f * (float(GetMouseY()) - selectedBall->centre.y);
+            }
 
-        std::vector<std::pair<Ball *, Ball *>> collidedPairs;
+            selectedBall = nullptr;
+        }
+
+        std::vector<std::pair<Ball *, Ball *>> collidedPairs; // remember pair of balls that have been collided
+
+        // update velocity
+        for (auto &ball : balls)
+        {
+            ball.accel.x = -ball.velocity.x * 0.8f;
+            ball.accel.y = -ball.velocity.y * 0.8f;
+
+            ball.velocity.x += ball.accel.x * fElapsedTime;
+            ball.velocity.y += ball.accel.y * fElapsedTime;
+
+            ball.centre.x += ball.velocity.x * fElapsedTime;
+            ball.centre.y += ball.velocity.y * fElapsedTime;
+
+            if (ball.centre.x < 0.0f)
+                ball.centre.x += ScreenWidth();
+            if (ball.centre.y < 0.0f)
+                ball.centre.y += ScreenHeight();
+            if (ball.centre.x > ScreenWidth())
+                ball.centre.x -= ScreenWidth();
+            if (ball.centre.y > ScreenHeight())
+                ball.centre.y -= ScreenHeight();
+
+            if ((ball.velocity.x * ball.velocity.x + ball.velocity.y * ball.velocity.y) < 0.01f)
+            {
+                ball.velocity.x = 0.0f;
+                ball.velocity.y = 0.0f;
+            }
+        }
 
         // naive approach
         for (auto &ball : balls)
@@ -124,6 +168,44 @@ private:
             }
         }
 
+        // dynamic resolution (takes place after the static one)
+        for (auto p : collidedPairs)
+        {
+            Ball *b1 = p.first;
+            Ball *b2 = p.second;
+            float D = sqrtf((b1->centre.x - b2->centre.x) * (b1->centre.x - b2->centre.x) + (b1->centre.y - b2->centre.y) * (b1->centre.y - b2->centre.y));
+
+            olc::vf2d normal = {(b2->centre.x - b1->centre.x) / D, (b2->centre.y - b1->centre.y) / D};
+            olc::vf2d tangent = {-normal.y, normal.x};
+
+            // tangent response
+            // float dpTan1 = b1->velocity.x * tangent.x + b1->velocity.y * tangent.y;
+            // float dpTan2 = b2->velocity.x * tangent.x + b2->velocity.y * tangent.y;
+
+            // normal response
+            // float dpNor1 = b1->velocity.x * normal.x + b1->velocity.y * normal.y;
+            // float dpNor2 = b2->velocity.x * normal.x + b2->velocity.y * normal.y;
+
+            // 1D elastic collision : https://en.wikipedia.org/wiki/Elastic_collision
+            // v_1 = ( u_1*(m_1-m_2) + 2*m_2*u_2 )/(m_1+m_2)
+            // v_2 = ( u_2*(m_2-m_1) + 2*m_1*u_2 )/(m_1+m_2)
+
+            // float m1 = (dpNor1 * (b1->mass - b2->mass) + 2 * b2->mass * dpNor2) / (b1->mass + b2->mass);
+            // float m2 = (dpNor2 * (b2->mass - b1->mass) + 2 * b1->mass * dpNor1) / (b1->mass + b2->mass);
+
+            // b1->velocity = {tangent.x * dpTan1 + m1 * normal.x, tangent.y * dpTan1 + m1 * normal.y};
+            // b2->velocity = {tangent.x * dpTan2 + m2 * normal.x, tangent.y * dpTan2 + m2 * normal.y};
+
+            // optimised wikipedia version
+            float kx = (b1->velocity.x - b2->velocity.x);
+            float ky = (b1->velocity.y - b2->velocity.y);
+            float p = 2.0 * (normal.x * kx + normal.y * ky) / (b1->mass + b2->mass);
+            b1->velocity = {b1->velocity.x - p * b2->mass * normal.x,
+                            b1->velocity.y - p * b2->mass * normal.y};
+            b2->velocity = {b2->velocity.x + p * b1->mass * normal.x,
+                            b2->velocity.y + p * b1->mass * normal.y};
+        }
+
         Clear(olc::DARK_BLUE);
 
         for (auto &b : balls)
@@ -141,6 +223,12 @@ private:
             DrawLine(b1->centre, b2->centre, olc::RED);
         }
 
+        if (selectedBall != nullptr)
+        {
+            olc::vf2d pos = {float(GetMouseX()), float(GetMouseY())};
+            DrawLine(selectedBall->centre, pos, olc::BLUE);
+        }
+
         return true;
     }
 };
@@ -149,7 +237,7 @@ int
 main()
 {
     Collisions demo;
-    if (demo.Construct(256, 100, 4, 4))
+    if (demo.Construct(300, 200, 3, 3))
         demo.Start();
 
     return 0;
