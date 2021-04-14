ba5acfa687d5500c24b2c679661e81b03ca2977e
Romain Boman <r.boman@uliege.be>
2021-04-14 18:21:54 +0200

    working sweep & prune

--- sandbox/onelonecoder/pixel/sweep_and_prune.cpp
+++ sandbox/onelonecoder/pixel/sweep_and_prune.cpp
@@ -4,6 +4,7 @@
 
 //#define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
+#include <algorithm>
 
 struct Box
 {
@@ -11,17 +12,18 @@ struct Box
     {
         pmin = c;
         pmax = c + olc::vf2d(Lx, Ly);
-        id = i; 
+        id = i;
     }
     olc::vf2d pmin;
     olc::vf2d pmax;
     int id;
 };
 
-
 class SweepAndPrune : public olc::PixelGameEngine
 {
-    std::vector<Box*> boxes;
+    std::vector<Box *> boxes;
+
+    int NBOXES = 10000;
 
 public:
     SweepAndPrune() { sAppName = "Sweep and Prune"; }
@@ -29,27 +31,85 @@ public:
 public:
     bool OnUserCreate() override
     {
-        int maxLx = 50;
-        int maxLy = 50;
-        int minLx = 20;
-        int minLy = 20;
+        int maxLx = 10;
+        int maxLy = 10;
+        int minLx = 5;
+        int minLy = 5;
 
-
-        for(int i=0; i<10; ++i)
-            boxes.push_back(new Box(i, {float(rand() % (ScreenWidth()-maxLx)), float(rand() % (ScreenHeight()-maxLy))}, 
-            rand() % (maxLx-minLx)+minLx, rand() % (maxLy-minLy)+minLy ) );
+        for (int i = 0; i < NBOXES; ++i)
+            boxes.push_back(new Box(i, {float(rand() % (ScreenWidth() - maxLx)), float(rand() % (ScreenHeight() - maxLy))},
+                                    rand() % (maxLx - minLx) + minLx, rand() % (maxLy - minLy) + minLy));
         return true;
     }
 
     bool OnUserUpdate(float fElapsedTime) override
     {
+
+        // sort boxes
+        std::vector<Box *> sBoxes = boxes;          // sorted list of boxes
+        std::vector<std::pair<Box *, Box *>> pairs; // possible collisions
+
+        if (true)
+        {
+            auto compFct = [](Box const *b1, Box const *b2) {
+                return b1->pmin.x < b2->pmin.x;
+            };
+
+            std::sort(sBoxes.begin(), sBoxes.end(), compFct);
+
+            if (GetKey(olc::Key::SPACE).bPressed && NBOXES < 100)
+            {
+                std::cout << "sorted list\n\t";
+                for (auto b : sBoxes)
+                {
+                    std::cout << b->id << ", ";
+                }
+                std::cout << '\n';
+            }
+
+            auto it = sBoxes.begin();
+            float xmin = (*it)->pmin.x;
+            for (; it != sBoxes.end(); ++it)
+            {
+                auto it2 = it;
+                while (++it2 != sBoxes.end())
+                {
+                    if ((*it2)->pmin.x <= (*it)->pmax.x)
+                    {
+                        // possible collision
+                        pairs.push_back({*it, *it2});
+                    }
+                    else
+                        break;
+                    // it2++;
+                }
+            }
+        }
+        if (GetKey(olc::Key::SPACE).bPressed)
+        {
+            std::cout << pairs.size() << " pairs found\n";
+            if (pairs.size() < 100)
+                for (auto p : pairs)
+                {
+                    std::cout << "\t(" << p.first->id << ", " << p.second->id << ")\n";
+                }
+            std::cout << '\n';
+            std::cout << pairs.size() << " tests to be done instead of " << NBOXES * (NBOXES - 1) << " (";
+            std::cout << float(pairs.size()) / (NBOXES * (NBOXES - 1)) * 100 << " %)\n";
+        }
+
+        // draw
+
         Clear(olc::DARK_BLUE);
 
-        for(auto b : boxes)
+        for (auto b : boxes)
         {
-            DrawRect(b->pmin, b->pmax-b->pmin, olc::WHITE);
-            DrawString( (b->pmin+b->pmax)/2-olc::vi2d(3,3), std::to_string(b->id), olc::YELLOW );
+            DrawRect(b->pmin, b->pmax - b->pmin, olc::WHITE);
+            if (NBOXES < 100)
+                DrawString((b->pmin + b->pmax) / 2 - olc::vi2d(3, 3), std::to_string(b->id), olc::YELLOW);
         }
+
+        DrawString(olc::vi2d(10, 10), "Press <SPACE>", olc::YELLOW);
 
         return true;
     }
@@ -59,7 +119,7 @@ int
 main()
 {
     SweepAndPrune demo;
-    if (demo.Construct(256, 240, 2, 2))
+    if (demo.Construct(1000, 500, 1, 1))
         demo.Start();
 
     return 0;
