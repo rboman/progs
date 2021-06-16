dce5c9deb3e28fdd3f4805455f5665306e33d8fc
Romain Boman <r.boman@uliege.be>
2021-06-16 17:24:07 +0200

    test stdin with fortran

--- sandbox/fortran/test_stdin.f90
+++ sandbox/fortran/test_stdin.f90
@@ -1,11 +1,50 @@
+!
+! test stdin with minGW.
+!
+! cmake -G "Unix Makefiles" ..
+! cmake --build . -- -j 3 && test_stdin.exe < ..\test_stdin.txt
+!
 
 program test_stdin
 
-    INTEGER :: i
-    CHARACTER(len=100) :: arg
+  CHARACTER(len=80) :: string
+  CHARACTER(len=80) :: ioerrmsg
+  real(8) :: val
 
-    DO i = 0, iargc()
-        CALL getarg(i, arg)
-        print *, 'arg', i, ' = "', arg, '"'
-    END DO
+    INTEGER :: iostatus
+
+    string = 'nop'
+
+    do while (string /= 'stop')
+        print *, '** enter a string, then [enter]  ("stop" to stop)'
+        read (*, '(A)', END=10, IOSTAT=iostatus, iomsg=ioerrmsg) string
+        print *, 'ioerrmsg = ', ioerrmsg
+        if (iostatus > 0) then
+
+          print *, 'cannot read STRING (iostatus = ', iostatus,')'
+        else if (iostatus < 0) then
+          print *, 'EOF (iostatus = ', iostatus,')'
+        else
+            ! read (*, '(A)', END=10) string
+            ! read (*, *, END=10) string  !  saute les lignes blanches
+            ! print *, '"', string, '"'
+            print *, 'string="', string, '"'
+        end if
+
+        ! read (string, '(F8.4)', END=10, IOSTAT=iostatus) val
+        read (string, fmt=*, IOSTAT=iostatus, iomsg=ioerrmsg) val
+        print *, 'ioerrmsg = ', ioerrmsg
+        ! print *, 'iostatus = ', iostatus
+        if (iostatus > 0) then
+            print *, 'cannot convert STRING to REAL*8 (iostatus = ', iostatus,')' 
+        else if (iostatus < 0) then
+          print *, 'EOF (iostatus = ', iostatus,')'  ! si la chaine est vide!
+        else
+            ! read (*, '(A)', END=10) string
+            ! read (*, *, END=10) string  !  saute les lignes blanches
+            ! print *, '"', string, '"'
+            print *, 'val = |', val, '|'
+        end if
+    end do
+10  continue
 end
