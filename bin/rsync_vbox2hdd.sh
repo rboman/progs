2ff20518f0261faf6819a7b195ab3e13cd7999f8
Romain Boman <romain.boman@gmail.com>
2021-11-04 16:39:23 +0100

    update/improve dropbox rsync script

--- bin/rsync_vbox2hdd.sh
+++ bin/rsync_vbox2hdd.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
-# script de backup rsync machines vbox de garfield => hdd
+# script de backup rsync machines vbox spirou => hdd portable
+
+VBOXFOLDER=/hdd3/boman/VirtualBox\ VMs
+HDDFOLDER=/media/boman/VBox
 
 echo "***** BACKUP Win10-x64 => hdd"
 
-rsync -avz \
+rsync -av --progress \
       --delete --delete-excluded \
-	  /home/boman/VirtualBox\ VMs/Win10-x64/ \
-	  /media/boman/VBox/VBox/Win10-x64/
+	  "${VBOXFOLDER}"/Win10-x64/ \
+	  "${HDDFOLDER}"/VBox/Win10-x64/
 
 echo
 echo "***** BACKUP Win10-x64-empty => hdd"
 
-rsync -avz \
+rsync -av --progress \
       --delete --delete-excluded \
-	  /home/boman/VirtualBox\ VMs/Win10-x64-empty/ \
-	  /media/boman/VBox/VBox/Win10-x64-empty/
+	  "${VBOXFOLDER}"/Win10-x64-empty/ \
+	  "${HDDFOLDER}"/VBox/VBox/Win10-x64-empty/
 
 echo
 echo "***** BACKUP Win10-x86 => hdd"
 
-rsync -avz \
+rsync -av --progress \
       --delete --delete-excluded \
-	  /home/boman/VirtualBox\ VMs/Win10-x86/ \
-	  /media/boman/VBox/VBox/Win10-x86/
+	  "${VBOXFOLDER}"/Win10-x86/ \
+	  "${HDDFOLDER}"/VBox/Win10-x86/
