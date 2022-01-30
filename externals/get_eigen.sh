86de005f505f31d514cec3eb7051497c9c2d055f
Romain Boman <r.boman@uliege.be>
2022-01-30 18:08:37 +0100

    add "get libs" scripts

---
+++ externals/get_eigen.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+VERSION=3.4.0
+
+wget -q https://gitlab.com/libeigen/eigen/-/archive/${VERSION}/eigen-${VERSION}.tar.gz
+tar xzf eigen-${VERSION}.tar.gz
+rm eigen-${VERSION}.tar.gz
+rm -rf eigen
+mv eigen-${VERSION} eigen
---
+++ externals/get_gmsh.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+VERSION=4.9.3
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    wget -q https://gmsh.info/bin/Linux/gmsh-${VERSION}-Linux64-sdk.tgz
+    tar xzf gmsh-${VERSION}-Linux64-sdk.tgz
+    rm gmsh-${VERSION}-Linux64-sdk.tgz
+    rm -rf gmsh-sdk
+    mv gmsh-${VERSION}-Linux64-sdk gmsh-sdk
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    wget -q https://gmsh.info/bin/MacOSX/gmsh-${VERSION}-MacOSX-sdk.tgz
+    tar xzf gmsh-${VERSION}-MacOSX-sdk.tgz
+    rm gmsh-${VERSION}-MacOSX-sdk.tgz
+    rm -rf gmsh-sdk
+    mv gmsh-${VERSION}-MacOSX-sdk gmsh-sdk
+else
+    echo "unknown system"
+fi
