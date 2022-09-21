a13c752ff2cbe2e24e74952325c66a152ce5a60a
Romain Boman <romain.boman@gmail.com>
2022-09-21 13:31:22 +0200

    Marc's code

---
+++ sandbox/fortran_oop/shapeSet_m.f90
@@ -0,0 +1,37 @@
+module shapeSet_m
+
+    use shape_m
+    use domainptr_m
+
+    implicit none
+
+    type shapeSet
+        type(domainptr), dimension(:), allocatable, private :: dom
+        !type (domainptr), dimension(:), allocatable :: dom
+    contains
+        procedure :: alloc, add, getdom
+    end type shapeSet
+
+contains
+
+    subroutine alloc(this, dim)
+        class(shapeSet), intent(inout) :: this
+        integer, intent(in) :: dim
+        allocate (this%dom(dim))
+    end subroutine alloc
+
+    subroutine add(this, ashape, i)
+        class(shapeSet), intent(inout) :: this
+        class(shape), pointer, intent(in) :: ashape
+        integer, intent(in) :: i
+        this%dom(i)%p => ashape
+    end subroutine add
+
+    function getdom(this, i)
+        class(shapeSet), intent(in) :: this
+        integer, intent(in) :: i
+        type(domainptr) :: getdom
+        getdom = this%dom(i)
+    end function getdom
+
+end module shapeSet_m
