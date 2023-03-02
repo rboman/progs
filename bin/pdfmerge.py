ea4c433733de120c37d35e27c845cd51956ea462
Romain Boman <romain.boman@gmail.com>
2023-03-02 11:52:57 +0100

    add extensions

--- bin/pdfmerge.py
+++ bin/pdfmerge.py
@@ -90,7 +90,7 @@ if __name__ == '__main__':
     tmpfiles = []
     for f in sys.argv[1:]:
         ext = os.path.splitext(f)[1]
-        if ext == '.py':
+        if ext in [ '.py', '.h', '.cpp' ] :
             # convert python files
             tmp = py2pdf(f)
             tmpfiles.append(tmp)
