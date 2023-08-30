0e5dd7af2c4f9a5aeebc9bdfea8736f60c243434
Romain Boman <romain.boman@gmail.com>
2023-08-30 17:31:43 +0200

    add vtk script for fixing debug installation of vtk 9.1.0

---
+++ bin/fix_debug_dll_names_vtk910.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+# fix VTK 9.1.0 debug python dll (.pyd) names after installation
+#   (e.g. vtkFiltersSources.cp310-win_amd64_d.pyd => vtkFiltersSources_d.pyd)
+
+import os
+vtkdir = r'C:\local\VTK-9.1.0'
+
+print("VTK dir =", vtkdir)
+
+pyddir = os.path.join(vtkdir, "bin", "Lib", "site-packages", "vtkmodules")
+if os.path.isdir(pyddir):
+    nfiles = 0
+    for f in os.listdir(pyddir):
+        if f.endswith('_d.pyd'):
+            if len(f.split('.')) == 3:
+                newf = f.split('.')[0]+'_d.pyd'
+                print(f, '=>', newf)
+                os.rename(os.path.join(pyddir, f), os.path.join(pyddir, newf))
+                nfiles += 1
+    print(nfiles, "files renamed.")
+else:
+    print("Error:", pyddir, "not found!")
