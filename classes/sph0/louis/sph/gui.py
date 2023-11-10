ecdb8e4e0d820c29fb05714ed09e4d484209bbc1
Romain Boman <r.boman@uliege.be>
2023-11-10 11:47:01 +0100

    make things work today with up-to-date libs and compilers

--- classes/sph0/louis/sph/gui.py
+++ classes/sph0/louis/sph/gui.py
@@ -1,94 +1,77 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Copyright 2020 University of Liège
-# 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
-#     http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
 # converts .res files to Paraview (VTK)
 
-
-
-
-
-import vtk
-version=vtk.vtkVersion().GetVTKMajorVersion()
 import glob
+import vtk
+version = vtk.vtkVersion().GetVTKMajorVersion()
+
 
 class ToParaview:
-    def __init__(self, verb = False):
+    def __init__(self, verb=False):
         self.verb = verb
-    
+
     def convertall(self, pattern='*MP*.res'):
         print("converting grid to VTK")
         self.convertGrid()
         print("converting %s to VTK" % pattern)
         for f in glob.glob(pattern):
             self.convertParts(f)
-            
+
     def convertGrid(self, fname='grid.out'):
-        if self.verb: print('converting', fname)
-        
+        if self.verb:
+            print('converting', fname)
+
         # read file
         file = open(fname)
         line = file.readline()
         nx, dx = line.strip().split()
         nx = int(nx)
         dx = float(dx)
-        #print 'nx=%d, dx=%f' % (nx,dx)
+        # print 'nx=%d, dx=%f' % (nx,dx)
         file.close()
-        
+
         # build a sgrid
-        grid  = vtk.vtkStructuredGrid()
+        grid = vtk.vtkStructuredGrid()
         points = vtk.vtkPoints()
-        grid.SetPoints(points)        
-        
-        for k in range(nx+1):
-            for j in range(nx+1):
-                for i in range(nx+1):
-                    points.InsertNextPoint(i*dx,j*dx,k*dx)
-        grid.SetDimensions(nx+1,nx+1,nx+1) 
-        
+        grid.SetPoints(points)
+
+        for k in range(nx + 1):
+            for j in range(nx + 1):
+                for i in range(nx + 1):
+                    points.InsertNextPoint(i * dx, j * dx, k * dx)
+        grid.SetDimensions(nx + 1, nx + 1, nx + 1)
+
         writer = vtk.vtkXMLStructuredGridWriter()
         compressor = vtk.vtkZLibDataCompressor()
         writer.SetCompressor(compressor)
         writer.SetDataModeToBinary()
-        if version>5:
+        if version > 5:
             writer.SetInputData(grid)
         else:
             writer.SetInput(grid)
-        writer.SetFileName(fname.replace('.out','.vts'))
-        writer.Write()        
-    
+        writer.SetFileName(fname.replace('.out', '.vts'))
+        writer.Write()
+
     def convertParts(self, fname):
-    
-        #if self.verb: 
+
+        # if self.verb:
         print('converting', fname)
         file = open(fname)
-    
-        ugrid  = vtk.vtkUnstructuredGrid()
+
+        ugrid = vtk.vtkUnstructuredGrid()
         points = vtk.vtkPoints()
         ugrid.SetPoints(points)
-        
-        codes = {'p': (1, "Pressure"), 
-                 'rho': (1, "Mass density"), 
-                 'v' : (3, "Velocity"), 
-                 'm': (1, "Mass"), 
+
+        codes = {'p': (1, "Pressure"),
+                 'rho': (1, "Mass density"),
+                 'v': (3, "Velocity"),
+                 'm': (1, "Mass"),
                  'c': (1, "Speed of sound"),
-                 'h': (1, "Smoothing length"), 
-                 'mu': (1, "max(mu_ab)"), 
-                 'nv': (1, "Nb of neighbours") }
+                 'h': (1, "Smoothing length"),
+                 'mu': (1, "max(mu_ab)"),
+                 'nv': (1, "Nb of neighbours")}
         results = {}
         for c in codes:
             scalars = vtk.vtkFloatArray()
@@ -97,17 +80,17 @@ class ToParaview:
             scalars.SetName(fullname)
             ugrid.GetPointData().AddArray(scalars)
             results[c] = scalars
-        
-        i=0
+
+        i = 0
         for line in file:
-            #x,y,z, vx,vy,vz, m, p = map(float, line.strip().split())
+            # x,y,z, vx,vy,vz, m, p = map(float, line.strip().split())
             try:
-                x,y,z, vx,vy,vz, rho, p, m, c, h, mu, nv = list(map(float, line.strip().split())) 
+                x, y, z, vx, vy, vz, rho, p, m, c, h, mu, nv = list(map(float, line.strip().split()))
             except:
-                print("**ERROR while reading file %s!\n\tline=\"%s\"" % (fname,line))
+                print("**ERROR while reading file %s!\n\tline=\"%s\"" % (fname, line))
                 break
-            points.InsertPoint(i, x,y,z)
-            results['v'].InsertNextTuple3(vx,vy,vz)
+            points.InsertPoint(i, x, y, z)
+            results['v'].InsertNextTuple3(vx, vy, vz)
             results['p'].InsertNextValue(p)
             results['rho'].InsertNextValue(rho)
             results['m'].InsertNextValue(m)
@@ -115,32 +98,34 @@ class ToParaview:
             results['h'].InsertNextValue(h)
             results['mu'].InsertNextValue(mu)
             results['nv'].InsertNextValue(nv)
-            i+=1
+            i += 1
         file.close()
-        
-        ntot=i
-        if self.verb: print("\t%d lines read. Converting grid to VTK..." % ntot)
-        
+
+        ntot = i
+        if self.verb:
+            print("\t%d lines read. Converting grid to VTK..." % ntot)
+
         for i in range(ntot):
             vertex = vtk.vtkVertex()
-            ids = vertex.GetPointIds() 
+            ids = vertex.GetPointIds()
             ids.SetId(0, i)
             ugrid.InsertNextCell(vertex.GetCellType(), ids)
-          
-        if self.verb:  
-            print("\t...", ugrid.GetNumberOfPoints(), 'points and', \
-            ugrid.GetNumberOfCells(), 'cells converted')
-    
+
+        if self.verb:
+            print("\t...", ugrid.GetNumberOfPoints(), 'points and',
+                  ugrid.GetNumberOfCells(), 'cells converted')
+
         writer = vtk.vtkXMLUnstructuredGridWriter()
         compressor = vtk.vtkZLibDataCompressor()
         writer.SetCompressor(compressor)
         writer.SetDataModeToBinary()
-        if version>5:
+        if version > 5:
             writer.SetInputData(ugrid)
         else:
             writer.SetInput(ugrid)
-        writer.SetFileName(fname.replace('.res','.vtu'))
+        writer.SetFileName(fname.replace('.res', '.vtu'))
         writer.Write()
 
-if __name__=="__main__":
-    ToParaview(verb=True).convertall()    
+
+if __name__ == "__main__":
+    ToParaview(verb=True).convertall()
--- classes/sph0/louis/sph/wutils.py
+++ classes/sph0/louis/sph/wutils.py
@@ -1,22 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2020 University of Liège
-# 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
-#     http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
 # Python utilities
-
 
 
 def setupwdir(testname):
@@ -24,22 +8,23 @@ def setupwdir(testname):
     creates a working folder for each test
     """
     import os, os.path
-    #print "__file__=",__file__
-    dir1=os.path.abspath(os.path.dirname(__file__)+os.sep+"..")+os.sep
-    print("dir1=",dir1)
-    print("testname=",testname)
-    common = os.path.commonprefix( (testname, dir1) )
-    #print "common=", common
-    resdir = testname[len(common):].replace(os.sep,"_")
-    resdir = os.path.splitext(resdir)[0] # remove ".py"
-    #print "resdir=", resdir    
-    wdir=os.path.join('workspace', resdir)
+    # print "__file__=",__file__
+    dir1 = os.path.abspath(os.path.dirname(__file__) + os.sep + "..") + os.sep
+    print("dir1=", dir1)
+    print("testname=", testname)
+    common = os.path.commonprefix((testname, dir1))
+    # print "common=", common
+    resdir = testname[len(common):].replace(os.sep, "_")
+    resdir = os.path.splitext(resdir)[0]  # remove ".py"
+    # print "resdir=", resdir
+    wdir = os.path.join('workspace', resdir)
     if not os.path.isdir(wdir):
         print("creating", wdir)
         os.makedirs(wdir)
     os.chdir(wdir)
-    
+
 # ------------------------------------------------------------------------------
+
 
 def parseargs():
     """
@@ -47,11 +32,11 @@ def parseargs():
     """
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("-v", "--verb", help="increase output verbosity", action="count", default=0)       
+    parser.add_argument("-v", "--verb", help="increase output verbosity", action="count", default=0)
     parser.add_argument("--nogui", help="disable any graphical output",
-                        action="store_true")    
+                        action="store_true")
     parser.add_argument("-k", help="nb of threads", type=int, default=1)
-    #parser.add_argument("-p", help="misc parameters")
+    # parser.add_argument("-p", help="misc parameters")
     parser.add_argument('file', nargs='*', help='python files')
-    args = parser.parse_args()     
+    args = parser.parse_args()
     return args
