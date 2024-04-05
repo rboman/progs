10ef8181e6684d336ee39d62e5c9e8a0b37732f1
Romain Boman <r.boman@uliege.be>
2024-04-05 18:44:27 +0200

    make things work with msys2

--- classes/louis/sph/__init__.py
+++ classes/louis/sph/__init__.py
@@ -1,7 +1,10 @@
+# init sph module
 
 # needed here because multiprocessing.Pool does not find the DLLs
 import platform, os, sys
 if platform.system() == 'Windows' and sys.version_info.minor >= 8:
+    # required for Python 3.8 (msvc) on windows
+    # (MSYS2 executes this, even it is not required)
     for v in os.environ['path'].split(';'):
         if os.path.exists(v):
             os.add_dll_directory(v)
