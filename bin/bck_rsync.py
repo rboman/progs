519132a8c83375ca0e3721342c47a7a180c3e000
Romain Boman <romain.boman@gmail.com>
2024-10-17 17:29:30 +0200

    add siemens server data to script

--- bin/bck_rsync.py
+++ bin/bck_rsync.py
@@ -1,19 +1,23 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Copie les repertoires rsync de garfield sur ma dropbox.
+#
+# Copie les répertoires rsync de "fsa-spirou" sur ma dropbox.
 # Ce script peut être lancé de n'importe où.
 
 import os, subprocess, shutil
 
 pars = {}
-pars['destdir'] = '/hdd2/boman/Dropbox/Backups/Repositories'
+pars['destdir'] = '/hdd2/boman/Dropbox/Backups/rsync'
 pars['srcdir'] = '/hdd2/boman/Backups/rsync'
-pars['folders'] = ['web']  # ['SVN', 'SVN2', 'web']
+pars['folders'] = [ 'web', 
+                   'siemens/web', 
+                   'siemens/FLMEXLM', 
+                   'siemens/LMS', 
+                   'siemens/LicenseServer' ]
 
 
-def tar_folder(fname):
-    bname = os.path.basename(fname)
-    arcname = '%s.tar.bz2' % bname
+def tar_folder(fname, tar_name):
+    arcname = '%s.tar.bz2' % tar_name
     if os.path.isfile(arcname):
         print('rm %s' % arcname)
         os.remove(arcname)
@@ -27,13 +31,11 @@ def tar_folder(fname):
         # parallel (requires 'pbzip2') - ?pxz does not always work ... and is very slow...
         cmd = ['tar', '-I', 'pbzip2', '-cf', arcname, fname]
     else:
-        print('\tinfo: use "apt-get install pbzip2" for parallel compression')
+        print('\tinfo: use "sudo apt install pbzip2" for parallel compression')
         cmd = ['tar', '-cjf', arcname, fname]
     # print cmd
     print(" ".join(cmd))
     subprocess.check_call(cmd)
-
-    # TODO: utiliser shutil.make_archive ????
 
     return arcname
 
@@ -42,7 +44,7 @@ def main(pars):
     destdir = pars['destdir']
     srcdir = pars['srcdir']
 
-    # check whether both folders exist
+    # check whether both src/dest folders exist
     if not os.path.isdir(destdir):
         raise Exception('destdir does not exist (%s)!' % destdir)
     if not os.path.isdir(srcdir):
@@ -53,10 +55,11 @@ def main(pars):
     os.chdir(srcdir)
 
     for folder in pars['folders']:
+        tar_name = folder.replace('/', '_')
         if not os.path.isdir(folder):
             raise Exception('folder does not exist (%s)!' % folder)
         # build archive
-        arcname = tar_folder(folder)
+        arcname = tar_folder(folder, tar_name)
         # copy archive to destination
         target = os.path.join(destdir, arcname)
         print('cp %s %s' % (arcname, target))
