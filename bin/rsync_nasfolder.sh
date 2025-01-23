a6bbcaf667214d4b47d2c2bfc5aa38d96035ec5e
Romain Boman <romain.boman@gmail.com>
2025-01-23 15:00:27 +0100

    rsync tests cédric

--- bin/rsync_nasfolder.sh
+++ bin/rsync_nasfolder.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
-# script de copie d'un repertoire du NAS vers garfield
+# script de copie d'un repertoire du distant vers mon PC en ssh via rsync
 
+# rsync -e ssh -avz \
+#       --delete --delete-excluded \
+#       --rsync-path=/usr/bin/rsync \
+#       rboman@metafor:/volume1/ftp/projects/Dominik_Boemer/ \
+#       /hdd2/boman/tmp/Dominik_Boemer/ 
+
+mkdir -p /hdd3/boman/Cedric_Laruelle/test2024
 rsync -e ssh -avz \
       --delete --delete-excluded \
       --rsync-path=/usr/bin/rsync \
-      rboman@metafor:/volume1/ftp/projects/Dominik_Boemer/ \
-      /hdd2/boman/tmp/Dominik_Boemer/ 
-
+      --exclude='*.res' \
+      --exclude='*.out' \
+      --exclude='*.pyc' \
+      --exclude='*.cfg' \
+      --exclude='*.txt' \
+      --exclude=workspace/ \
+      --exclude=__pycache__/ \
+      boman@fabulous:/home/laruelle/test2024/ \
+      /hdd3/boman/Cedric_Laruelle/test2024
+cd /hdd3/boman/Cedric_Laruelle
+tar cvJf test2024.tar.xz test2024
